d0e886086658eb4cef1b226f5ca2bf3c542d4d56
aprillove <aprilclove86@gmail.com>
2022-03-01 22:21:41 -0500

    updated and added authorization and authentication

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,11 @@ import boto3
 import uuid
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import FeedingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 ##fake database to follow
 
 S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
@@ -14,8 +19,10 @@ def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+
+@login_required
 def cats_index(request):
-    cats = Cat.objects.all()
+    cats = Cat.objects.filter(user = request.user)
     return render(request, 'cats/index.html', {'cats': cats})
 def toys_index(request):
     toys = Toy.objects.all()
@@ -68,27 +75,48 @@ def add_photo(request, cat_id):
             print('*********************')
     return redirect('detail', cat_id=cat_id)
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
 
-class CatCreate(CreateView):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'invalid sign up- please try again'
+
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message }
+    return render(request, 'registration/signup.html', context)
+
+
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
-    fields = '__all__'
-class ToyCreate(CreateView):
+    fields = ('name', 'breed', 'age', 'description')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
     # fat models, skinny controllers
 
-class CatUpdate(UpdateView):
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     fields = ('name', 'breed', 'description', 'age')
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ('name', 'color')
 
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     success_url ='/cats/'
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url ='/toys/'
